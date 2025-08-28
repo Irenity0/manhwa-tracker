53eccb8df019be0e0d82396bed5b3d3b57153f59
Afra Anjum <lilithkard77@gmail.com>
2025-08-28 19:18:42 +0600

    simple tweak

--- src/components/ManhwaTable.tsx
+++ src/components/ManhwaTable.tsx
@@ -315,9 +315,6 @@ const ManhwaTable = () => {
 
       {/* Footer */}
       <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="text-muted-foreground flex-1 text-sm">
-          {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
         <div className="space-x-2">
           <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
             Previous
